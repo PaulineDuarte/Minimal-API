519c4d5f04de2092c13906c1d9a0b746cc4bf5c4
Pauline Duarte <Paulineduarte1@hotmail.com>
2024-11-06 18:31:05 -0300

    Adiconar o banco de dados

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,6 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Dominio.Entidades;
+
+
+
 namespace MinimalAPI.Infraestrutura.Db; 
 
-public class DbContexto 
+public class DbContexto : DbContext
 {
-    
+    private readonly IConfiguration _configuracaoAppSettings;
+    public DbContexto(IConfiguration configuracaoAppSettings)
+    {
+        _configuracaoAppSettings = configuracaoAppSettings;
+    }
+    public DbSet<Administrador> Administradores {get; set;}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var stringConexao= _configuracaoAppSettings.GetConnectionString("ConexaoString").ToString();
+        if(!string.IsNullOrEmpty(stringConexao))
+        {
+            optionsBuilder.UseMySql(stringConexao,ServerVersion.AutoDetect(stringConexao));
+        } 
+        
+    }
 }
