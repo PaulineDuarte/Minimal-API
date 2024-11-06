e662c3e1516d586812eeb2118359823652284018
Pauline Duarte <Paulineduarte1@hotmail.com>
2024-11-06 18:32:20 -0300

    Add o builder

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using MinimalAPI.DTOs;
+using MinimalAPI.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options => {
+    options.UseMySql(builder.Configuration.GetConnectionString("ConexaoString"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("ConexaoString")));
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
