50c4fb3fa6fe6e88f048b302858c88bb2b469211
Pauline Duarte <Paulineduarte1@hotmail.com>
2024-11-07 17:19:27 -0300

    Post Criar Veiculo

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using Minimal_API.Dominio.interfaces;
 using Minimal_API.Dominio.Servicos;
+using MinimalAPI.Dominio.Entidades;
 using MinimalAPI.Dominio.ModelsViews;
 using MinimalAPI.DTOs;
 using MinimalAPI.Infraestrutura.Db;
 
+#region Builder
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,23 +24,42 @@ builder.Services.AddDbContext<DbContexto>(options => {
 });
 
 var app = builder.Build();
+#endregion
 
-
+#region Home
 app.MapGet("/", () => Results.Json(new Home()));
+#endregion 
 
 
-app.MapPost("/login",([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico)=> 
+#region Login Administrador
+
+app.MapPost("/administradores/login",([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico)=> 
 {
     	if(administradorServico.Login(loginDTO) != null) 
             return Results.Ok("Login com sucesso");
         else 
             return Results.Unauthorized();
 });
+#endregion
 
+#region Veiculos 
+app.MapPost("/veiculos",([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico)=> 
+{       //nova instancia de veiculos
+        
+    	var veiculo = new Veiculo
+        {
+            Nome = veiculoDTO.Nome,
+            Marca = veiculoDTO.Marca,
+            Ano = veiculoDTO.Ano
+        };
+        veiculoServico.Incluir(veiculo);
+        return Results.Created($"/veiculo/{veiculo.Id}",veiculo); 
+});
+#endregion
 
-
+#region App
 app.UseSwagger();
 app.UseSwaggerUI(); 
 
 app.Run();
-
+#endregion
