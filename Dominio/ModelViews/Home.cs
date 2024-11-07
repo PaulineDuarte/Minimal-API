42f28dd40025e68689288ce1f8695f6e7ed291a2
Pauline Duarte <Paulineduarte1@hotmail.com>
2024-11-06 21:36:10 -0300

    Criar a rota Home

---
+++ Dominio/ModelViews/Home.cs
@@ -0,0 +1,10 @@
+
+
+namespace MinimalAPI.Dominio.ModelsViews; 
+
+
+public struct Home
+{
+    public String Mensagem { get => "Bem Vindo a API de Veiculos"; }   
+    public String Doc { get => "/swagger"; }   
+}
