152d41d0ba478865cc85bc098c9789ec9ab4fd82
Pauline Duarte <Paulineduarte1@hotmail.com>
2024-11-07 17:44:16 -0300

    update

--- Dominio/Interfaces/IVeiculoServico.cs
+++ Dominio/Interfaces/IVeiculoServico.cs
@@ -5,7 +5,7 @@ namespace Minimal_API.Dominio.interfaces ;
 
 public interface IVeiculoServico
 {
-    List<Veiculo> Todos(int pagina , string nome , string marca);
+    List<Veiculo> Todos(int pagina=1 , string nome=null , string marca=null);
     Veiculo BuscaPorId(int Id);
 
     void Incluir(Veiculo veiculo) ; 
